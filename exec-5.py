aa6c4972854070fe6ab16c1ac6ab782da96fd31d
f4-Lucas <lcezario126@gmail.com>
2024-10-03 21:04:37 -0300

    Create exec-5.py

---
+++ exec-5.py
@@ -0,0 +1,10 @@
+Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+#contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída
+
+print("ALunos      Nota")
+
+print ("Aline       9")
+print ("Mário       7")
+print ("Sérgio      9")
+print ("Shirley     8")
